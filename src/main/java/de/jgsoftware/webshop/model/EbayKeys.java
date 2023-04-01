1ad86b4519de0861d341c98a29e6a5f065bafd22
hoscho <jgeist@gmx.net>
2023-04-01 13:35:47 +0200

    update model EBayKeys

--- src/main/java/de/jgsoftware/webshop/model/EbayKeys.java
+++ src/main/java/de/jgsoftware/webshop/model/EbayKeys.java
@@ -4,29 +4,23 @@ public class EbayKeys
 {
     public static String ebayappid;
     public static String ebaytoken;
-
     public static String certid;
 
     public String getEbayappid() {
         return ebayappid;
     }
-
     public void setEbayappid(String ebayappid) {
         this.ebayappid = ebayappid;
     }
-
     public String getEbaytoken() {
         return ebaytoken;
     }
-
     public void setEbaytoken(String ebaytoken) {
         this.ebaytoken = EbayKeys.ebaytoken;
     }
-
     public String getCertid() {
         return certid;
     }
-
     public void setCertid(String certid) {
         this.certid = certid;
     }
