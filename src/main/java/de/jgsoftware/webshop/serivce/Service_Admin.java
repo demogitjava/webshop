28f9bc7cadb63981dfa4d6603cdf8052029432f0
hoscho <jgeist@gmx.net>
2023-04-05 12:42:20 +0200

    add ebay oauth source as package

--- src/main/java/de/jgsoftware/webshop/serivce/Service_Admin.java
+++ src/main/java/de/jgsoftware/webshop/serivce/Service_Admin.java
@@ -8,6 +8,16 @@ import org.springframework.stereotype.Service;
 import de.jgsoftware.webshop.serivce.interfaces.i_service_admin;
 import de.jgsoftware.webshop.dao.interfaces.shop.i_dao_admin;
 
+
+import com.ebay.api.client.auth.oauth2.CredentialUtil;
+import com.ebay.api.client.auth.oauth2.OAuth2Api;
+import com.ebay.api.client.auth.oauth2.model.AccessToken;
+import com.ebay.api.client.auth.oauth2.model.OAuthResponse;
+import com.ebay.api.client.auth.oauth2.model.Environment;
+import com.ebay.api.client.auth.oauth2.model.RefreshToken;
+import com.ebay.api.client.auth.oauth2.model.TokenResponse;
+import com.ebay.api.client.auth.oauth2.model.TokenType;
+
 /**
  *
  * @author hoscho
@@ -20,6 +30,13 @@ public class Service_Admin implements i_service_admin
     i_dao_admin iDaoAdmin;
 
 
+    OAuth2Api ebayoauth;
+    CredentialUtil ebaycredentialutil;
+    AccessToken ebayaccesstoken;
+    com.ebay.api.client.auth.oauth2.model.Environment ebayenvironment;
+
+    com.ebay.api.client.auth.oauth2.model.Environment environment;
+
     @Override
     public i_dao_admin getiDaoAdmin() {
         return iDaoAdmin;
@@ -29,5 +46,16 @@ public class Service_Admin implements i_service_admin
     @Override
     public void setiDaoAdmin(i_dao_admin iDaoAdmin) {
         this.iDaoAdmin = iDaoAdmin;
+    }
+
+
+    public void startEbayClient()
+    {
+        ebayoauth = new OAuth2Api();
+
+        ebaycredentialutil = new CredentialUtil();
+        ebayaccesstoken = new AccessToken();
+
+
     }
 }
