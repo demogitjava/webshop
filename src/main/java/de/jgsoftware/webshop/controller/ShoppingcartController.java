7ef86f226b00aff1678b7ca60b5c1c9fd4da6828
hoscho <jgeist@gmx.net>
2023-04-11 17:13:39 +0200

    add model for table useragent to save http.request to table

--- src/main/java/de/jgsoftware/webshop/controller/ShoppingcartController.java
+++ src/main/java/de/jgsoftware/webshop/controller/ShoppingcartController.java
@@ -16,6 +16,8 @@ import de.jgsoftware.webshop.model.shoppingcart;
 
 import de.jgsoftware.webshop.serivce.interfaces.i_shoppingcart_service;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  *
  * @author hoscho
@@ -28,48 +30,64 @@ public class ShoppingcartController implements i_shoppingcart
 
     Principal principal;
 
+    String stusername;
+
+    List<Users> lsusers;
 
     @Autowired
     i_shoppingcart_service shoppingcart_service;
 
+    @Autowired
+    HttpServletRequest request;
 
     @Override
     public String addToCart(@PathVariable("productId") String productId, Principal principal, Model model)
     {
 
 
-        String stusername = principal.getName();
 
 
+        if(principal == null)
+        {
 
-        /*
-            return enabled user from
-            demodb table users
+            System.out.print("user is not loggedin " + "\n");
 
 
-         */
-        List<Users> lsusers = (List<Users>) shoppingcart_service.getShcart().getloggedinuserdata(stusername);
+        }
+        else
+        {
+            stusername = principal.getName();
 
-        /*
+            /*
+                return enabled user from
+                demodb table users
+
+
+             */
+            lsusers = (List<Users>) shoppingcart_service.getShcart().getloggedinuserdata(stusername);
+            /*
 
                     private int id;
                     private int user_id;
                     private long product_id;
-         */
-        shoppingcart mshopingcart = new shoppingcart();
+            */
+            shoppingcart mshopingcart = new shoppingcart();
 
-        // get id from table users
-        long idusers = lsusers.get(0).getId();
-        mshopingcart.setUser_id((int) idusers);
-        mshopingcart.setProduct_id(Long.parseLong(productId));
-
-
-
-        List<shoppingcart> shlist = new ArrayList<shoppingcart>();
-        shlist.add(mshopingcart);
+            // get id from table users
+            long idusers = lsusers.get(0).getId();
+            mshopingcart.setUser_id((int) idusers);
+            mshopingcart.setProduct_id(Long.parseLong(productId));
 
 
-        shoppingcart_service.getShcart().saveprodcut(shlist);
+
+            List<shoppingcart> shlist = new ArrayList<shoppingcart>();
+            shlist.add(mshopingcart);
+
+
+            shoppingcart_service.getShcart().saveprodcut(shlist);
+
+        }
+
 
         return "redirect:/";
     }
