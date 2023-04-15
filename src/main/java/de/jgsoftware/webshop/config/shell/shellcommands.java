6db566af22deb47f3a2679038e7fb2a7b6542a85
hoscho <jgeist@gmx.net>
2023-04-15 13:58:12 +0200

    add method in shellcommand
    add method for create h2 cluster from webshop to
    target source on an other container in my case
    the ip is resived to 192.168.178.6
    
    the help method list the simple commands
    
    the help cluster print out the h2 command to
    cluster the database

--- src/main/java/de/jgsoftware/webshop/config/shell/shellcommands.java
+++ src/main/java/de/jgsoftware/webshop/config/shell/shellcommands.java
@@ -17,6 +17,42 @@ import org.springframework.shell.standard.ShellMethod;
 public class shellcommands
 {
 
+    @ShellMethod("helpcluster")
+    public String helpcluster()
+    {
+        System.out.print("start h2 cluster" + "\n");
+        System.out.print("----------------" + "\n");
+        System.out.print("to start the cluster on the source maschine " + "\n");
+        System.out.print("\n");
+        System.out.print("\n");
+        System.out.print("java org.h2.tools.CreateCluster" + "\n");
+        System.out.print("-urlSource jdbc:h2:tcp://localhost:9101/~/demodb" + "\n");
+        System.out.print("-urlTarget jdbc:h2:tcp://localhost:9102/~/demodb" + "\n");
+        System.out.print("-user root" + "\n");
+        System.out.print("-password jj78mvpr52k1" + "\n");
+        System.out.print("-serverList 192.168.178.5:9101,192.168.178.6:9102" + "\n");
+
+        System.out.print("------------------------" + "\n");
+        System.out.print("192.168.178.5 -> webshop" + "\n");
+        System.out.print("192.168.178.6 -> target h2 container" + "\n");
+
+        System.out.print("the url: http://www.h2database.com/html/advanced.html#clustering" + "\n");
+
+
+
+        return "----------------------";
+    }
+
+    @ShellMethod("help")
+    public String help()
+    {
+        System.out.print("\n");
+        System.out.print("cluster h2 database -> helpcluster" + "\n");
+        System.out.print("create a h2 cluster -> startcluster ->  urlsource urltarget user password serverlocal servertaget" + "\n");
+
+
+        return "----------------------";
+    }
 
     @ShellMethod("create h2 cluster --->  startcluster urlSource urlTaget user password serverList")
     public CreateCluster startcluster(String sturlsource, String sturltarget, String stuser, String stpassword, String stservlocal, String stservtarget)
