9d1c4314dd067728a74e75276a5dd5987231c933
hoscho <jgeist@gmx.net>
2023-04-24 19:37:47 +0200

    .

--- src/main/java/de/jgsoftware/webshop/dao/Dao_Ebay.java
+++ src/main/java/de/jgsoftware/webshop/dao/Dao_Ebay.java
@@ -21,34 +21,7 @@ public class Dao_Ebay implements i_dao_ebay
     @Override
     public void Ebaystartclient()
     {
-        ApiContext apiContext = new ApiContext();
-        ApiCredential cred = apiContext.getApiCredential();
 
-        if(EbayKeys.ebaytoken == null)
-        {
-            System.out.print("================" + "\n");
-            System.out.print("no ebay token is in memory" + "\n");
-            System.out.print("================" + "\n");
-        }
-
-        // model EbayKeys
-        cred.seteBayToken(EbayKeys.ebaytoken);
-
-        // url for sandbox
-        // https://api.sandbox.ebay.com/wsapi.
-
-        /*
-                get String from
-
-
-                de.jgsoftware.webshop.config.EbayConfig
-
-                if null = Sandbox
-                if String is set = Production mode
-         */
-        apiContext.setApiServerUrl(EBayConfig.ebayserverurl);
-
-        System.out.print("apiContext " + "\n" + apiContext);
 
     }
 }
