777c41bd7e51e07f1fb1f589b8aed0c567703bb5
hoscho <jgeist@gmx.net>
2023-09-17 19:59:17 +0200

    edit interface i_index_dao

--- src/main/java/de/jgsoftware/webshop/dao/interfaces/shop/i_Index_Dao.java
+++ src/main/java/de/jgsoftware/webshop/dao/interfaces/shop/i_Index_Dao.java
@@ -1,6 +1,7 @@
 package de.jgsoftware.webshop.dao.interfaces.shop;
 
 
+import de.jgsoftware.landingpage.model.jpa.demodb.Webtextlayout;
 import de.jgsoftware.webshop.model.m_webtextlayout;
 import de.jgsoftware.webshop.model.useragent;
 
@@ -14,6 +15,6 @@ import java.util.List;
 public interface i_Index_Dao
 {
     
-   List<m_webtextlayout> getPageLanguageText();
+   List<Webtextlayout> getPageLanguageText();
    useragent saveuseragent(useragent muagent);
 }
