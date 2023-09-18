42181476b9b0a186965a20dd491ed7c8a43b12db
hoscho <jgeist@gmx.net>
2023-09-18 21:34:10 +0200

    add @Repository to i_jpa_useragent

--- src/main/java/de/jgsoftware/webshop/dao/interfaces/demodb/i_jpa_useragent.java
+++ src/main/java/de/jgsoftware/webshop/dao/interfaces/demodb/i_jpa_useragent.java
@@ -2,11 +2,13 @@ package de.jgsoftware.webshop.dao.interfaces.demodb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import de.jgsoftware.webshop.model.jpa.demodb.Useragent;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author hoscho
  */
+@Repository
 public interface i_jpa_useragent extends JpaRepository<Useragent, Integer>{
     
 }
