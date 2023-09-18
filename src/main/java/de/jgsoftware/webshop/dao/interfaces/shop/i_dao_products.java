579c7763808b2be3f5aa7905999627b051ad23d7
hoscho <jgeist@gmx.net>
2023-09-18 21:13:58 +0200

    edit i_dao_products
    edit path for query to jpa model

--- src/main/java/de/jgsoftware/webshop/dao/interfaces/shop/i_dao_products.java
+++ src/main/java/de/jgsoftware/webshop/dao/interfaces/shop/i_dao_products.java
@@ -1,10 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package de.jgsoftware.webshop.dao.interfaces.shop;
 
-import de.jgsoftware.webshop.model.Product;
+import de.jgsoftware.webshop.model.jpa.shopdb.Products;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -19,23 +15,23 @@ public interface i_dao_products
         load all products with selectected 1 
         in database
     */
-    List<Product> getProductsforLandingpage();
+    List<Products> getProductsforLandingpage();
     
     /*
         select product by id
     */
-    Product getProductById(long productId);
+    Products getProductById(long productId);
     
     /*
         search product over Textfield 
         in the webshop
     */
-    List<Product> searchProductovertextfield(String searchProduct, Pageable pageable);
+    List<Products> searchProductovertextfield(String searchProduct, Pageable pageable);
     
     
     /*
         search product for 
         pages display in webshop
     */
-    List<Product> searchProductop25(String searchProduct);
+    List<Products> searchProductop25(String searchProduct);
 }
