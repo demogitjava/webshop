73beb43035a99be7693417019ba423b4453ad9c9
hoscho <jgeist@gmx.net>
2023-09-18 22:07:32 +0200

    edit model Sprachen
    add import jakarta.persistence.*;

--- src/main/java/de/jgsoftware/webshop/model/jpa/demodb/Sprachen.java
+++ src/main/java/de/jgsoftware/webshop/model/jpa/demodb/Sprachen.java
@@ -2,9 +2,11 @@ package de.jgsoftware.webshop.model.jpa.demodb;
 
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+
+import jakarta.persistence.*;
 
 /**
  *
