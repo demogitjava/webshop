3295a26cabdc8b08fbd5f1557766d21369ecdfab
hoscho <jgeist@gmx.net>
2023-09-18 22:06:31 +0200

    edit model Kundenstamm add import jakarta.persistence.*;

--- src/main/java/de/jgsoftware/webshop/model/jpa/demodb/Kundenstamm.java
+++ src/main/java/de/jgsoftware/webshop/model/jpa/demodb/Kundenstamm.java
@@ -1,13 +1,15 @@
 package de.jgsoftware.webshop.model.jpa.demodb;
 
-import javax.persistence.Entity;
+//import javax.persistence.Entity;
 
 
 import java.io.Serializable;
 import java.util.Date;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+
+import jakarta.persistence.*;
 
 /**
  *
