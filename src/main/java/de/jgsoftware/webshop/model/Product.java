2c6e2e441d68e37e0a61287dd9903ff253d90d52
hoscho <jgeist@gmx.net>
2023-09-18 22:16:45 +0200

    edit model Products add import jakarta.persistence.*;

--- src/main/java/de/jgsoftware/webshop/model/Product.java
+++ src/main/java/de/jgsoftware/webshop/model/Product.java
@@ -1,14 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package de.jgsoftware.webshop.model;
-
-
 
 import org.springframework.format.annotation.NumberFormat;
 
-import javax.persistence.*;
+//import javax.persistence.*;
+
+import jakarta.persistence.*;
 /**
  *
  * @author hoscho
